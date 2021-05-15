b0526f10383231c8873b5050c709086ba3c8d169
Juan Uribe <juanuribeo13@gmail.com>
2021-05-15 09:11:53 -0500

    Create Xcode project

---
+++ Appgate/AppgateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppgateApp.swift
+//  Appgate
+//
+//  Created by Juan Uribe on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AppgateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
